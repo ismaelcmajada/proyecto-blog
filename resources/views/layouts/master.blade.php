9e3e1047832537697d9d56a334e881612ad4e708
vagrant <vagrant@homestead>
2020-02-05 14:14:32 +0000

    Introducción de la plantilla, partials y layouts.

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <title>Clean Blog - Start Bootstrap Theme</title>
+
+  <!-- Bootstrap core CSS -->
+  <link rel="stylesheet" href="{{asset('css/app.css')}}"> 
+
+  <!-- Custom fonts for this template -->
+  <link href="{{asset('fontawesome-free/css/all.min.css')}}" rel="stylesheet" type="text/css">
+  <link href='https://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
+  <link href='https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
+
+  <!-- Custom styles for this template -->
+  <link href="{{asset('css/clean-blog.min.css')}}" rel="stylesheet">
+
+</head>
+
+<body>
+
+  <!-- Navigation -->
+  @include('partials.navbar')
+
+  <!-- Page Header -->
+  @yield('header')
+
+  <!-- Main Content -->
+  @yield('content')
+
+  <!-- Footer -->
+  @include('partials.footer')
+
+  <!-- Bootstrap core JavaScript -->
+  <script src="{{asset('jquery/jquery.min.js')}}"></script>
+  <script src="{{asset('js/app.js')}}"></script>
+
+  <!-- Custom scripts for this template -->
+  <script src="{{asset('js/clean-blog.min.js')}}"></script>
+
+</body>
+
+</html>
