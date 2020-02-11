c89cfebd2297a64a9a8d7ff7a0e77026c9124502
vagrant <vagrant@homestead>
2020-02-11 09:17:51 +0000

    Migrations.

---
+++ database/migrations/2020_02_11_090332_create_posts_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('title');
+            $table->string('subtitle');
+            $table->longText('content');
+            $table->bigInteger('category_id');
+            $table->bigInteger('author_id');
+            $table->timestamps();
+
+            $table->foreign('category_id')->references('id')->on('categorys');
+            $table->foreign('author_id')->references('id')->on('authors');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
