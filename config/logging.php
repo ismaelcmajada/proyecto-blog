5d222098dd7b68ae4a8683c1ef5cef337cac6252
vagrant <vagrant@homestead>
2020-02-29 16:51:52 +0000

    Error logs para heroku y trusting the load balancer

--- config/logging.php
+++ config/logging.php
@@ -34,16 +34,15 @@ return [
     |
     */
 
+    
     'channels' => [
+
         'stack' => [
             'driver' => 'stack',
             'channels' => ['single'],
-            'ignore_exceptions' => false,
         ],
-
         'single' => [
-            'driver' => 'single',
-            'path' => storage_path('logs/laravel.log'),
+            'driver' => 'errorlog',
             'level' => 'debug',
         ],
 
